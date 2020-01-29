dcda1d016aa85552c5f0161b2b415ff9c446a490
Samantha Landen <sam@rosewater.tech>
2020-01-29 18:25:14 -0500

    day3: Solution for part 2
    I'm sure there's a more efficient solution here, but I can't think of any
    that aren't totally unreadable, which is not what I'm trying to accomplish

--- day3/spiral.py
+++ day3/spiral.py
@@ -1,5 +1,6 @@
 import math
 
+# Part 1
 def computeManhattanDistance(start):
     shellDimension = getNextOddRoot(start)
     stepsIn = math.floor(shellDimension / 2)
@@ -16,3 +17,39 @@ def getMidpoints(root):
     square = root * root
     midpointLength = math.floor(root / 2)
     return map(lambda x: square - (midpointLength * x), [1, 3, 5, 7])
+
+# Part 2
+def findFirstLargerValue(input):
+    spiral = {(0,0): 1}
+    coordinate = (0, 0)
+    level = 0
+    last = 1
+
+    while last < input:
+        (currX, currY) = coordinate
+        if currY == -1 * level:
+            coordinate = (currX + 1, currY)
+        elif currX == level and currY < level:
+            coordinate = (currX, currY + 1)
+        elif currY == level and currX > (-1 * level):
+            coordinate = (currX - 1, currY)
+        else:
+            coordinate = (currX, currY - 1)
+        
+        if coordinate == (level + 1, -1 * level):
+            level += 1
+        
+        last = sumNeighbors(spiral, coordinate)
+        spiral[coordinate] = last
+
+    return last
+
+def sumNeighbors(spiral, coordinate):
+    sum = 0
+    coordX, coordY = coordinate
+    for x in range(coordX - 1, coordX + 2):
+        for y in range(coordY - 1, coordY + 2):
+            # NOTE: it's unnecessary to exclude coordinate itself here since it won't have a value
+            if (x, y) in spiral:
+                sum += spiral[(x, y)]
+    return sum
